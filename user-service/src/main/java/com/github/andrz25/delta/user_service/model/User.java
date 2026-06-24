8f4f8d64fc7115a92488c9f251fa959ceac433d0
Andre Zamudio <zamudioandre2506@gmail.com>
2026-06-23 21:32:52 -0400

    feat: added user model field validation

--- user-service/src/main/java/com/github/andrz25/delta/user_service/model/User.java
+++ user-service/src/main/java/com/github/andrz25/delta/user_service/model/User.java
@@ -8,6 +8,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
@@ -20,21 +26,37 @@ public class User {
     @SequenceGenerator(name = "user_seq_gen", sequenceName = "user_seq", initialValue = 1, allocationSize = 50)
     private Long id;
 
+    @NotBlank(message = "Full name cannot be blank")
+    @Size(max = 255, message = "Full name must not exceed 255 characters")
+    @Pattern(regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ' -]+$", message = "Invalid full name format")
     @Column(name = "full_name", nullable = false, length = 255)
     private String fullName;
 
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 to 50 characters")
+    @Pattern(regexp = "^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$", message = "Invalid username format")
     @Column(name = "username", nullable = false, unique = true, length = 50)
     private String username;
 
+    @NotBlank(message = "Email address cannot be blank")
+    @Email(message = "A valid email address must be provided")
+    @Size(max = 255, message = "Email address must not exceed 255 characters")
     @Column(name = "email_address", nullable = false, unique = true, length = 255)
     private String email;
 
+    @NotNull
+    @Past
     @Column(name = "date_of_birth", nullable = false)
     private LocalDate dateOfBirth;
 
+    // TODO: Use libphonenumber for validation
+    @NotBlank(message = "Phone number must not be null or blank")
+    @Size(min = 7, max = 20, message = "Phone number must be between 7-20 characters")
+    @Pattern(regexp = "^\\+?[1-9]\\d{1,14}$", message = "Invalid phone number format")
     @Column(name = "phone_number", nullable = false, unique = true, length = 20)
     private String phoneNumber;
 
+    @NotNull(message = "Role must not be null")
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
     private Role role;
