3f8eb8de51cba3beb83745f25f14b0e2655739ff
Jhef Andre Zamudio <zamudioandre2506@gmail.com>
2026-08-30 21:59:09 -0400

    refactor: remove redundant fields and setters
    DuplicateResourceException already has a field method which made
    DuplicateEmailException.email and DuplicateUsernameException.username
    fields obsolete

--- user-service/src/main/java/com/github/andrz25/delta/user_service/exception/DuplicateUsernameException.java
+++ user-service/src/main/java/com/github/andrz25/delta/user_service/exception/DuplicateUsernameException.java
@@ -1,16 +1,7 @@
 package com.github.andrz25.delta.user_service.exception;
 
 public class DuplicateUsernameException extends DuplicateResourceException {
-
-    private final String username;
-
     public DuplicateUsernameException(String username) {
-        this.username = username;
-
         super("User", "username", username);
-    }
-
-    public String getUsername() {
-        return username;
     }
 }
